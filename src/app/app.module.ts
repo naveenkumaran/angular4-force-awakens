79eea93333f39038f2d211a9c10a47a0eaae3382
naveenkumaran.a <naveenkumaran.a@gmail.com>
2017-08-07 06:39:49 +0530

    create routing using ng generate module route --routing --module=app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouteRoutingModule } from './route/route-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
