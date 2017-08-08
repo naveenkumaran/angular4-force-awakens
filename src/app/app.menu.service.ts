11a521598cdaa0176bad1c706e8fc426245b6d43
naveenkumaran.a <naveenkumaran.a@gmail.com>
2017-08-08 06:21:54 +0530

    create menu service and added md

---
+++ src/app/app.menu.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+
+export interface MenuItem {
+
+}
+
+
+@Injectable()
+export class AppMenuService {
+
+  
+  items: Array<MenuItem>;
+  
+  constructor() { 
+
+    
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { RouteRoutingModule } from './route/route-routing.module';
+import { AppMenuService } from './app.menu.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { RouteRoutingModule } from './route/route-routing.module';
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     RouteRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
